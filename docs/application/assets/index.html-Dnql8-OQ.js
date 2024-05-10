b88cb7a2dc39722b8649e08d396ed61fb8a4daac
fleshcat <42990634+fleshcat@users.noreply.github.com>
2024-05-10 18:51:18 +0800

    update

--- docs/application/assets/index.html-Dnql8-OQ.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r as a,o as n,c as o,d as c}from"./app-B8QQgiXE.js";const r={};function l(s,_){const e=a("Catalog");return n(),o("div",null,[c(e)])}const i=t(r,[["render",l],["__file","index.html.vue"]]),p=JSON.parse('{"path":"/phd/","title":"Phd","lang":"en-US","frontmatter":{"title":"Phd"},"headers":[],"git":{},"filePathRelative":null,"excerpt":""}');export{i as comp,p as data};
